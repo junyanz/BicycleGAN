80133e423ecb9e77c2688d3f7f612082275fbd7d
junyanz <junyanz@berkeley.edu>
2018-01-16 17:13:44 -0800

    add facades models

--- README.md
+++ README.md
@@ -110,9 +110,9 @@ Download the pre-trained models with the following script.
 ```bash
 bash ./pretrained_models/download_model.sh model_name
 ```
-- `edges2shoes` (edge -> photo): trained on UT Zappos50K dataset.
-- `night2day` (nighttime scene -> daytime scene): trained on around 100 [webcams](http://transattr.cs.brown.edu/). Please use the test script `./scripts/test_night2day.sh` for this model.
-
+- `edges2shoes` (edge -> photo) trained on UT Zappos50K dataset.
+- `night2day` (nighttime scene -> daytime scene) trained on around 100 [webcams](http://transattr.cs.brown.edu/): download: `bash ./datasets/download_testset.sh night2day`; test:  `bash ./scripts/test_night2day.sh`
+- `facades_label2image` (facade label -> facade photo) trained on the CMP Facades dataset: download: `bash ./datasets/download_testset.sh facades`; test:  `bash ./scripts/test_facades.sh`
 
 More models are coming soon!
 
---
+++ scripts/test_facades.sh
@@ -0,0 +1,31 @@
+# models
+RESULTS_DIR='./results/facades'
+G_PATH='./pretrained_models/facades_label2image_net_G.pth'
+E_PATH='./pretrained_models/facades_label2image_net_E.pth'
+
+# dataset
+CLASS='facades'
+DIRECTION='BtoA' # from domain A to domain B
+LOAD_SIZE=286 # scale images to this size
+FINE_SIZE=256 # then crop to this size
+INPUT_NC=3  # number of channels in the input image
+
+# misc
+GPU_ID=1   # gpu id
+HOW_MANY=10 # number of input images duirng test
+NUM_SAMPLES=10 # number of samples per input images
+
+
+# command
+CUDA_VISIBLE_DEVICES=${GPU_ID} python ./test.py \
+  --dataroot ./datasets/${CLASS} \
+  --results_dir ${RESULTS_DIR} \
+  --G_path ${G_PATH} \
+  --E_path ${E_PATH} \
+  --which_direction ${DIRECTION} \
+  --loadSize ${FINE_SIZE} --fineSize ${FINE_SIZE} \
+  --input_nc ${INPUT_NC} \
+  --how_many ${HOW_MANY} \
+  --n_samples ${NUM_SAMPLES} \
+  --center_crop \
+  --no_flip
