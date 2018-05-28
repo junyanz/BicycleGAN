910f964ab55da65e99d523ddb68452093337e8c9
junyanz <junyanzhu89@gmail.com>
2018-05-27 22:59:24 -0400

    add bicyclegan simple model

--- scripts/train_simple.sh
+++ scripts/train_simple.sh
@@ -1,6 +1,6 @@
 set -ex
-MODEL='bicycle_gan'
-CLASS=${1}  # facades, day2night, edges2shoes, edges2handbags, maps
+MODEL='bicycle_gan_simple'
+CLASS=${1}      # facades, day2night, edges2shoes, edges2handbags, maps
 GPU_ID=${2}
 DISPLAY_ID=$((GPU_ID*10+1))
 NZ=8
@@ -21,36 +21,36 @@ INPUT_NC=3
 # dataset parameters
 case ${CLASS} in
 'facades')
-  NITER=200
-  NITER_DECAY=200
-  SAVE_EPOCH=25
+  NITER=100
+  NITER_DECAY=100
+  SAVE_EPOCH=20
   DIRECTION='BtoA'
   ;;
 'edges2shoes')
-  NITER=30
-  NITER_DECAY=30
+  NITER=15
+  NITER_DECAY=15
   LOAD_SIZE=256
   SAVE_EPOCH=5
   INPUT_NC=1
   NO_FLIP='--no_flip'
   ;;
 'edges2handbags')
-  NITER=15
-  NITER_DECAY=15
+  NITER=10
+  NITER_DECAY=5
   LOAD_SIZE=256
   SAVE_EPOCH=5
   INPUT_NC=1
   ;;
 'maps')
-  NITER=200
-  NITER_DECAY=200
+  NITER=100
+  NITER_DECAY=100
   LOAD_SIZE=600
   SAVE_EPOCH=25
   DIRECTION='BtoA'
   ;;
 'day2night')
-  NITER=50
-  NITER_DECAY=50
+  NITER=25
+  NITER_DECAY=25
   SAVE_EPOCH=10
   ;;
 *)
@@ -71,6 +71,7 @@ CUDA_VISIBLE_DEVICES=${GPU_ID} python ./train.py \
   --loadSize ${LOAD_SIZE} \
   --fineSize ${FINE_SIZE} \
   --nz ${NZ} \
+  --init_type kaiming \
   --input_nc ${INPUT_NC} \
   --niter ${NITER} \
   --niter_decay ${NITER_DECAY} \
